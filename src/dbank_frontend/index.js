bb7ea7708328d2cd6d472cd8912c45fd3b254df1
austinmusuya <amusuya57@gmail.com>
2025-02-06 06:21:54 +0300

    still working on the frontend.

--- src/dbank_frontend/index.js
+++ src/dbank_frontend/index.js
@@ -1,5 +1,5 @@
 import { dbank_backend } from "../declarations/dbank_backend";
 
 window.addEventListener("load", function () {
-  console.log("Finished loading!");
+  alert("Finished loading!");
 });
